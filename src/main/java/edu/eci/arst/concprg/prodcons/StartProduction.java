7938429706f923b02de466e3774c51cd3a9d6c88
unknown <juan.cbecerra@labinfo.is.escuelaing.edu.co>
2024-08-29 08:16:33 -0500

    Parte I

--- src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
@@ -21,9 +21,9 @@ public class StartProduction {
     public static void main(String[] args) {
         
         Queue<Integer> queue=new LinkedBlockingQueue<>();
-        
-        
-        new Producer(queue,Long.MAX_VALUE).start();
+
+        //Long.MAX_VALUE
+        new Producer(queue, 1).start();
         
         //let the producer create products for 5 seconds (stock).
         try {
